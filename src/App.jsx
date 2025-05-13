eabc9a7ab304483268ff5c516dafe3e67a3fbfb0
katlegomaphango <mphangokatlego08@gmail.com>
2025-05-13 17:58:25 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/header'
 import Competencies from './components/Competencies'
+import Projects from './components/Projects'
 
 function App() {
 
@@ -9,6 +10,20 @@ function App() {
       <div>
         <Header />
         <Competencies />
+        <Projects />
+
+        <div className="footer">
+          <div className="footer_msg">
+            Let’s chat! I’m ready to work on exciting projects.        
+          </div>
+          <div className="footer_links">
+            <a href="mailto:mphangokatlego08@gmail.com">mphangokatlego08@gmail.com</a> |
+            <a href="tel:+27685882974">068 588 2974</a> |
+            <a href="https://github.com/katlegomaphango">Github</a> |
+            <a href="https://www.linkedin.com/in/kat-maphango/">linkedIn</a> |
+            <a href="https://kativan.netlify.app/">Personal Website</a>
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/components/Projects.jsx
@@ -0,0 +1,85 @@
+const Projects = () => {
+    return (
+        <>
+        <div className='projects'>
+            <h2 className="title">Projects</h2>
+            <div className="projects-container">
+                <div className="project">
+                    <div className="project_header">
+                        <div>Year</div><div>Heart</div>
+                    </div>
+                    <h3 className="project_title">title</h3>
+                    <p>
+                        <strong>Brief</strong> ipsum, dolor sit amet consectetur 
+                        adipisicing elit. Harum natus recusandae 
+                        nulla facere iure obcaecati ipsa odit, odio quas ab!
+                    </p>
+                    <p>
+                        <strong>Tech:</strong> Lorem ipsum dolor sit.
+                    </p>
+                    <p>
+                        <strong>Responsibilities</strong> Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Unde, asperiores.
+                    </p>
+                </div>
+                <div className="project">
+                    <div className="project_header">
+                        <div>Year</div><div>Heart</div>
+                    </div>
+                    <h3 className="project_title">title</h3>
+                    <p>
+                        <strong>Brief</strong> ipsum, dolor sit amet consectetur 
+                        adipisicing elit. Harum natus recusandae 
+                        nulla facere iure obcaecati ipsa odit, odio quas ab!
+                    </p>
+                    <p>
+                        <strong>Tech:</strong> Lorem ipsum dolor sit.
+                    </p>
+                    <p>
+                        <strong>Responsibilities</strong> Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Unde, asperiores.
+                    </p>
+                </div>
+                <div className="project">
+                    <div className="project_header">
+                        <div>Year</div><div>Heart</div>
+                    </div>
+                    <h3 className="project_title">title</h3>
+                    <p>
+                        <strong>Brief</strong> ipsum, dolor sit amet consectetur 
+                        adipisicing elit. Harum natus recusandae 
+                        nulla facere iure obcaecati ipsa odit, odio quas ab!
+                    </p>
+                    <p>
+                        <strong>Tech:</strong> Lorem ipsum dolor sit.
+                    </p>
+                    <p>
+                        <strong>Responsibilities</strong> Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Unde, asperiores.
+                    </p>
+                </div>
+                <div className="project">
+                    <div className="project_header">
+                        <div>Year</div><div>Heart</div>
+                    </div>
+                    <h3 className="project_title">title</h3>
+                    <p>
+                        <strong>Brief</strong> ipsum, dolor sit amet consectetur 
+                        adipisicing elit. Harum natus recusandae 
+                        nulla facere iure obcaecati ipsa odit, odio quas ab!
+                    </p>
+                    <p>
+                        <strong>Tech:</strong> Lorem ipsum dolor sit.
+                    </p>
+                    <p>
+                        <strong>Responsibilities</strong> Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Unde, asperiores.
+                    </p>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Projects
